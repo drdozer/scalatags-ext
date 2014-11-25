0014b3bd5b4537dbe36f515cdb468726999a7439
Matthew Pocock <turingatemyhamster@gmail.com>
2014-11-25 16:20:17 +0000

    Now builds with the simplified project layout. Version bumped to 0.1.1.

--- project/ScalatagsExtBuild.scala
+++ project/ScalatagsExtBuild.scala
@@ -9,52 +9,25 @@ import ScalaJSKeys._
 
 
 object ScalatagsExtBuild extends Build {
-  val prj = "scalajs-ext"
-  
-  val modules = file(".") 
+
+  val module = XModule(id = "scalatags-ext", defaultSettings = buildSettings)
+
   val logger = ConsoleLogger()
 
   lazy val buildSettings: Seq[Setting[_]] = Seq(
     organization := "uk.co.turingatemyhamster",
     scalaVersion := "2.11.4",
     crossScalaVersions := Seq("2.11.4", "2.11.2"),
-    scalacOptions ++= Seq("-deprecation", "-unchecked")
+    scalacOptions ++= Seq("-deprecation", "-unchecked"),
+    version := "0.1.1"
   )
 
-  // We want an empty root, i.e. no source in ",", nothing published
-
- lazy val prjModule = Project(
-    id =  prj ,
-    base =  modules,
-    settings = buildSettings
-  ).aggregate(sharedjvm, sharedjs, prjJs, prjJvm)
-    .dependsOn(sharedjvm, prjJvm, sharedjs, prjJs)
-
-  lazy val prjJvm: Project = Project(
-    id = s"${prj}_jvm",
-    base =  modules / "jvm",
-    settings = buildSettings ++ scalajsJvmSettings 
-  ).dependsOn(sharedjvm).aggregate(sharedjvm)
-
-  lazy val prjJs: Project = Project(
-    id = s"${prj}_js",
-    base = modules / "js",
-    settings = buildSettings ++ scalajsJsSettings
-  ).enablePlugins(SbtScalajs)
-    .dependsOn(sharedjs).aggregate(sharedjs)
-
-  lazy val sharedjvm = Project(
-    id = s"${prj}_shared_jvm",
-    base =  modules / "shared",
-    settings = buildSettings ++ scalajsJvmSettings ++
-      Dependencies.scalarx ++ Dependencies.scalatags
-  )
-
-  lazy val sharedjs = Project(
-    id = s"${prj}_shared_js",
-    base =  modules / ".shared_js",
-    settings =  buildSettings ++ scalajsJsSettings ++ linkedSources(sharedjvm) ++
-      Dependencies.scalarx ++ Dependencies.scalatags ++ Dependencies.sclalajsDom
-  ).enablePlugins(SbtScalajs)
+  lazy val scalatagsExt           = module.project(scalatagsExtJvm, scalatagsExtJs)
+  lazy val scalatagsExtJvm        = module.jvmProject(scalatagsExtSharedJvm)
+  lazy val scalatagsExtJs         = module.jsProject(scalatagsExtSharedJs)
+  lazy val scalatagsExtSharedJvm  = module.jvmShared().settings(
+      Dependencies.scalarx ++ Dependencies.scalatags : _*)
+  lazy val scalatagsExtSharedJs   = module.jsShared(scalatagsExtSharedJvm).settings(
+        Dependencies.scalarx ++ Dependencies.scalatags ++ Dependencies.sclalajsDom : _*)
 
 }
