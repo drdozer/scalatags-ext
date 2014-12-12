3fce3597b842ee914627dfaaaf57aa64fa6ae851
Matthew Pocock <turingatemyhamster@gmail.com>
2014-12-12 22:52:03 +0000

    Updated the project build to publish to maven and have better dependencies and things.

--- project/ScalatagsExtBuild.scala
+++ project/ScalatagsExtBuild.scala
@@ -6,6 +6,7 @@ import com.inthenow.sbt.scalajs.SbtScalajs._
 import scala.scalajs.sbtplugin.ScalaJSPlugin._
 import ScalaJSKeys._
 import bintray.Plugin._
+import bintray.Keys._
 import org.eclipse.jgit.lib._
 
 object ScalatagsExtBuild extends Build {
@@ -22,9 +23,14 @@ object ScalatagsExtBuild extends Build {
     crossScalaVersions := Seq("2.11.4", "2.10.4"),
     scalacOptions ++= Seq("-deprecation", "-unchecked"),
     version := makeVersion(baseVersion),
-    publishMavenStyle := false,
-//    bintray.Keys.repository in bintray.Keys.bintray := "sbt-plugins",
-    bintray.Keys.bintrayOrganization in bintray.Keys.bintray := None,
+    resolvers += Resolver.url(
+      "scalajs Rep",
+      url("http://dl.bintray.com/content/scala-js/scala-js-releases"))(
+      Resolver.ivyStylePatterns),
+    resolvers += "drdozer Bintray Repo" at "http://dl.bintray.com/content/drdozer/maven",
+    publishMavenStyle := true,
+    repository in bintray := "maven",
+    bintrayOrganization in bintray := None,
     licenses +=("Apache-2.0", url("http://www.apache.org/licenses/LICENSE-2.0.html"))
   )
 
@@ -32,7 +38,8 @@ object ScalatagsExtBuild extends Build {
     packageBin in Compile  := file(""))
   lazy val scalatagsExtJvm        = module.jvmProject(scalatagsExtSharedJvm).
     settings(scalatagsExtPlatformJvmSettings : _*)
-  lazy val scalatagsExtJs         = module.jsProject(scalatagsExtSharedJs)
+  lazy val scalatagsExtJs         = module.jsProject(scalatagsExtSharedJs).
+    settings(scalatagsExtPlatformJsSettings : _*)
   lazy val scalatagsExtSharedJvm  = module.jvmShared().
     settings(scalatagsExtSharedJvmSettings : _*)
   lazy val scalatagsExtSharedJs   = module.jsShared(scalatagsExtSharedJvm).
@@ -40,21 +47,28 @@ object ScalatagsExtBuild extends Build {
 
   lazy val scalatagsExtPlatformJvmSettings = Seq(
     libraryDependencies ++= Seq(
-      "org.scalacheck" %% "scalacheck" % "1.12.1" % "test"
+      "org.scalacheck" %% "scalacheck" % "1.12.1" % "test",
+      "uk.co.turingatemyhamster" %% "scalarx_shared" % "0.2.6"
+    )
+  )
+
+  lazy val scalatagsExtPlatformJsSettings = Seq(
+    libraryDependencies ++= Seq(
+      "uk.co.turingatemyhamster" %%% "scalarx_shared" % "0.2.6"
     )
   )
 
   lazy val scalatagsExtSharedJvmSettings = Seq(
     libraryDependencies ++= Seq(
       "com.scalatags" %% "scalatags" % "0.4.1",
-      "uk.co.turingatemyhamster" %% "scalarx" % "0.2.6"
+      "uk.co.turingatemyhamster" %% "scalarx_shared" % "0.2.6"
     )
   )
 
   lazy val scalatagsExtSharedJsSettings = Seq(
     libraryDependencies ++= Seq(
       "com.scalatags" %%% "scalatags" % "0.4.1",
-      "uk.co.turingatemyhamster" %%% "scalarx" % "0.2.6",
+      "uk.co.turingatemyhamster" %%% "scalarx_shared" % "0.2.6",
       "org.scala-lang.modules.scalajs" %%% "scalajs-dom" % "0.6"
     )
   )
@@ -76,7 +90,7 @@ object ScalatagsExtBuild extends Build {
 
   def makeVersion(baseVersion: String): String = {
     val branch = fetchGitBranch()
-    if(branch == "main") {
+    if(branch == "master") {
       baseVersion
     } else {
       val tjn = Option(System.getenv("TRAVIS_JOB_NUMBER"))
